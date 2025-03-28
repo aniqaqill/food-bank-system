8b7f5a8e12a6328c2bc79eba7e470590c4ad85c9
Aniq Aqil <aniqaqil06@gmail.com>
2025-03-28 18:59:52 +0800

    update

--- src/Authentication.java
+++
@@ -1,6 +0,0 @@
-public interface Authentication{
-    public void setUsername(String username);
-    public void setPassword(String password);
-    public String getUsername();
-    public String getPassword();
-}
--- src/Contributor.java
+++
@@ -1,7 +0,0 @@
-public class Contributor extends Person{
-    //constructor
-    public Contributor(String fullName, String phoneNumber, String email, String username, String password , 
-                String street, String city, String state, int postalCode){
-        super(fullName, phoneNumber, email, username, password, street, city, state, postalCode);
-    }
-}
